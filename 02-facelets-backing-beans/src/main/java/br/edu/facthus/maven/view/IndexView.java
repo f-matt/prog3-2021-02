1066ebf81a6f68f6a0f92afde5649fa441bd97d0
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-09-14 21:33:07 -0300

    Configuração Ajax

--- 02-facelets-backing-beans/src/main/java/br/edu/facthus/maven/view/IndexView.java
+++ 02-facelets-backing-beans/src/main/java/br/edu/facthus/maven/view/IndexView.java
@@ -7,6 +7,31 @@ import javax.inject.Named;
 @RequestScoped
 public class IndexView {
 	
+	private String texto = "Hello facelets!";
 	
+	private String mensagem;
 	
+	public void atualizaMensagem() {
+		texto = String.format("Você digitou: %s", mensagem);
+	}
+
+	/*
+	 * Auto-generated
+	 */
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+		
 }
